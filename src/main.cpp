dde8b783d73566294f84614a2f71e5ddca1ab4b3
Lee <fitzy2011@gmail.com>
2017-08-04 20:49:36 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+
+#include "mqtt/async_client.h"
+
+using namespace std;
+
+const long Timeout = 10000L;
+
+int main(int argc, char* argv[])
+{
+    mqtt::async_client client(argv[1], "test_00");
+
+    mqtt::callback cb;
+    client.set_callback(cb);
+
+    mqtt::token_ptr connectOk = client.connect();
+    cout << "Connecting.." << endl;
+
+    connectOk->wait_for(Timeout);
+
+    cout << "Connection Established!" << endl;
+
+    // Now try with itemized publish.
+    const char *payload = "";
+    mqtt::token_ptr publishOk = client.publish("test/topic", payload, strlen(payload) + 1, 1, false);
+    publishOk->wait_for(Timeout);
+    cout << "Payload sent!" << endl;
+
+    return 0;
+}
